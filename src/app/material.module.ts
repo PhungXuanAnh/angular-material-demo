766c6f2a4f15d135e548e504f6f118ebe83a7782
Phung Xuan Anh <phungxuananh1991@gmail.com>
2019-09-19 17:13:43 +0700

    Refactoring material module

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,17 +1,39 @@
 import { NgModule } from '@angular/core';
-import {MatNativeDateModule,MatSnackBarModule,MatIconModule,MatDialogModule, MatButtonModule, MatTableModule, MatPaginatorModule , MatSortModule,MatTabsModule, MatCheckboxModule, MatToolbarModule, MatCard, MatCardModule, MatFormField, MatFormFieldModule, MatProgressSpinnerModule, MatInputModule } from '@angular/material';
 
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatSelectModule} from '@angular/material/select';
-import {MatSliderModule} from '@angular/material/slider';
-import {MatDividerModule} from '@angular/material/divider';
+import { MatNativeDateModule, MatSnackBarModule, MatIconModule } from '@angular/material';
+import { MatDialogModule, MatButtonModule, MatTableModule } from '@angular/material';
+import { MatPaginatorModule, MatSortModule, MatTabsModule } from '@angular/material';
+import { MatCheckboxModule, MatToolbarModule, MatCard } from '@angular/material';
+import { MatCardModule, MatFormField, MatFormFieldModule } from '@angular/material';
+import { MatProgressSpinnerModule, MatInputModule } from '@angular/material';
+
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
 
-  imports: [MatTabsModule,MatDividerModule,MatSliderModule,MatSelectModule,MatRadioModule,MatNativeDateModule,MatDatepickerModule,MatSnackBarModule,MatIconModule,MatDialogModule,MatProgressSpinnerModule,MatButtonModule,MatSortModule,MatTableModule,MatTabsModule, MatCheckboxModule, MatToolbarModule, MatCardModule, MatFormFieldModule, MatProgressSpinnerModule, MatInputModule, MatPaginatorModule],
-  exports: [MatTabsModule,MatDividerModule,MatSliderModule,MatSelectModule,MatRadioModule,MatNativeDateModule,MatDatepickerModule,MatSnackBarModule,MatIconModule,MatDialogModule,MatProgressSpinnerModule,MatButtonModule,MatSortModule, MatCheckboxModule, MatToolbarModule, MatCardModule,MatTableModule,MatTabsModule, MatFormFieldModule, MatProgressSpinnerModule, MatInputModule, MatPaginatorModule],  
-  
+  imports: [
+    MatTabsModule, MatDividerModule, MatSliderModule,
+    MatSelectModule, MatRadioModule, MatNativeDateModule,
+    MatDatepickerModule, MatSnackBarModule, MatIconModule,
+    MatDialogModule, MatProgressSpinnerModule, MatButtonModule,
+    MatSortModule, MatTableModule, MatTabsModule, MatCheckboxModule,
+    MatToolbarModule, MatCardModule, MatFormFieldModule, MatProgressSpinnerModule,
+    MatInputModule, MatPaginatorModule
+  ],
+  exports: [
+    MatTabsModule, MatDividerModule, MatSliderModule,
+    MatSelectModule, MatRadioModule, MatNativeDateModule,
+    MatDatepickerModule, MatSnackBarModule, MatIconModule,
+    MatDialogModule, MatProgressSpinnerModule, MatButtonModule,
+    MatSortModule, MatCheckboxModule, MatToolbarModule, MatCardModule,
+    MatTableModule, MatTabsModule, MatFormFieldModule, MatProgressSpinnerModule,
+    MatInputModule, MatPaginatorModule
+  ],
+
 })
 export class MyMaterialModule { }
 
