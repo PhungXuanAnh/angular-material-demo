e52868dd2396b095bef471970db5e8921dd2f8d1
Phung Xuan Anh <phungxuananh1991@gmail.com>
2019-09-19 16:02:13 +0700

    Add webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        polyfills: "./src/polyfills.ts",
+        main: "./src/main.ts",
+    },
+    resolve: {
+        extensions: ['.js', '.ts']
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'), // output directory
+        filename: "[name].js" // name of the generated bundle
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                loader: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+                loader: ['url-loader?limit=100000']
+            },
+            {
+                test: /\.ts$/,
+                loader: "awesome-typescript-loader"
+            },
+            {
+                test: /\.ts$/,
+                enforce: "pre",
+                loader: 'tslint-loader'
+            },
+            {
+                test: /\.scss$/,
+                loader: ["raw-loader", "sass-loader?sourceMap"]
+            },
+            {
+                test: /\.html$/,
+                loader: "html-loader"
+            },
+        ],
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    chunks: 'initial',
+                    name: 'vendor',
+                    test: 'vendor',
+                },
+            },
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "src/index.html",
+            inject: "body"
+        }),
+        new webpack.ContextReplacementPlugin(
+            /\@angular(\\|\/)core(\\|\/)fesm5/,
+            path.resolve(__dirname, 'src'), {}
+        )
+    ],
+    devtool: "source-map",
+    devServer: {
+        historyApiFallback: true
+    }
+};
